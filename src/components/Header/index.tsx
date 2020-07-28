60e6ab7e1f31856ae5db403823a71d6f4133990c
Marcus Pereira <marcus.work@hotmail.com>
2020-07-28 16:41:03 -0300

    Changing structure to receive a icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { FiMapPin } from 'react-icons/fi';
+
 import { Container } from './styles';
 
 const Header: React.FC = () => {
   return (
     <>
       <Container>
-        <h1>Consultar cep</h1>
+        <header>
+          <FiMapPin size={30} />
+          <h1>Consultar cep</h1>
+        </header>
       </Container>
     </>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,4 +5,18 @@ export const Container = styled.div`
   color: #f4f4f4;
   margin-bottom: 60px;
   padding: 20px;
+  display: flex;
+  justify-content: center;
+
+  header {
+    display: flex;
+    max-width: 960px;
+    padding: 0 20px;
+    width: 100%;
+
+    svg {
+      color: #ffd400;
+      margin-right: 8px;
+    }
+  }
 `;
